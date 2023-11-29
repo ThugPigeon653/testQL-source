1c14f8283b8388f5f192920faa48fda07181c740
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-30 08:53:16 +1000

    corrected linux installer

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -3,6 +3,5 @@ chmod +x dist/set-path.sh
 pip install pyinstaller
 pyinstaller --onefile testQL.py
 pyinstaller --onefile --add-data "config.json;." testQL.py
-mv set-path.bat dist/set-path.bat
-source dist/set-path.sh
-pause
+mv set-path.bat dist/set-path.sh
+./dist/set-path.sh
